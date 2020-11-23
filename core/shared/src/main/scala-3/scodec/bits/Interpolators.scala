9ed683efcfd8bcae8cfe484224c1b0da18d409a4
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-11-23 09:57:19 +0100

    Remove unnecessary names for Quotes

--- core/shared/src/main/scala-3/scodec/bits/Interpolators.scala
+++ core/shared/src/main/scala-3/scodec/bits/Interpolators.scala
@@ -44,7 +44,7 @@ private def hexInterpolator(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[
 extension (inline ctx: StringContext) inline def bin (inline args: BitVector*): BitVector =
   ${binInterpolator('ctx, 'args)}
 
-private def binInterpolator(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[BitVector]])(using qctx: Quotes): Expr[BitVector] = {
+private def binInterpolator(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[BitVector]])(using Quotes): Expr[BitVector] = {
   (strCtxExpr, argsExpr) match {
     case ('{ StringContext(${Varargs(parts)}: _*) }, Varargs(args)) =>
       val partValues: Seq[String] = parts.map { case p @ Const(part) =>
